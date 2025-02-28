da91eb0b793d6e3dc0937c6a439ad0619e3dc742
ayush-karki <118434743+ayush-karki@users.noreply.github.com>
2025-02-28 09:12:05 +0000

    syntax updated for className

--- moj/src/components/CaseSummary.jsx
+++ moj/src/components/CaseSummary.jsx
@@ -11,13 +11,13 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Case Name:</span>
+              <span className="title">Case Name:</span>
               <span>Include the full name of the case (e.g., Brown v. Board of Education).</span>
             </p>
           </li>
           <li>
             <p>
-              <span class="title">Citation:</span>
+              <span className="title">Citation:</span>
               <span>Provide the legal citation (e.g., 347 U.S. 483 (1954)).</span>
             </p>
           </li>
@@ -28,13 +28,13 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Background:</span>
+              <span className="title">Background:</span>
               <span>Briefly introduce the context and background of the case.</span>
             </p>
           </li>
           <li>
             <p>
-              <span class="title">Parties Involved: </span>
+              <span className="title">Parties Involved: </span>
               <span>Name the parties involved in the case (plaintiff vs. defendant).</span>
             </p>
           </li>
@@ -45,13 +45,13 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Situation Overview:</span>
+              <span className="title">Situation Overview:</span>
               <span>Outline the key facts and events leading up to the legal dispute.</span>
             </p>
           </li>
           <li>
             <p>
-              <span class="title">Relevant Details:</span>
+              <span className="title">Relevant Details:</span>
               <span>Highlight any specific details that are crucial to understanding the case's context.</span>
             </p>
           </li>
@@ -62,7 +62,7 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Questions at Hand:</span>
+              <span className="title">Questions at Hand:</span>
               <span>Identify the main legal questions or issues that the court needed to resolve.</span>
             </p>
           </li>
@@ -73,13 +73,13 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Plaintiff's Arguments:</span>
+              <span className="title">Plaintiff's Arguments:</span>
               <span> Summarize the main arguments or claims made by the plaintiff.</span>
             </p>
           </li>
           <li>
             <p>
-              <span class="title">Defendant's Arguments:</span>
+              <span className="title">Defendant's Arguments:</span>
               <span> Summarize the main arguments or claims made by the defendant.</span>
             </p>
           </li>
@@ -90,13 +90,13 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Court's Decision:</span>
+              <span className="title">Court's Decision:</span>
               <span>State the outcome of the case (who won and the verdict).</span>
             </p>
           </li>
           <li>
             <p>
-              <span class="title">Legal Reasoning:</span>
+              <span className="title">Legal Reasoning:</span>
               <span>Explain the court's reasoning and the legal principles applied in reaching its decision.</span>
             </p>
           </li>
@@ -107,13 +107,13 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Implications:</span>
+              <span className="title">Implications:</span>
               <span>Discuss the case's implications for future legal interpretations, law, or society.</span>
             </p>
           </li>
           <li>
             <p>
-              <span class="title">Precedents Set:</span>
+              <span className="title">Precedents Set:</span>
               <span> If applicable, mention any legal precedents the case established.</span>
             </p>
           </li>
@@ -124,7 +124,7 @@ export default function CaseSummary() {
         <ul>
         <li>
             <p>
-              <span class="title">Summary:</span>
+              <span className="title">Summary:</span>
               <span>Conclude with a brief recap of the case's importance and its impact on the legal landscape.</span>
             </p>
           </li>
@@ -135,7 +135,7 @@ export default function CaseSummary() {
         <ul>
           <li>
             <p>
-              <span class="title">Sources:</span>
+              <span className="title">Sources:</span>
               <span> List any additional sources or references used in preparing the case summary.</span>
             </p>
           </li>
