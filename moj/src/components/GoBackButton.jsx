b709a5bf1c8cf2fe8f8709801e438e7ca535742c
Ross Baker <ross.baker@wyser.online>
2025-03-11 14:42:41 +0000

    back button home if dashboard else -1

--- moj/src/components/GoBackButton.jsx
+++ moj/src/components/GoBackButton.jsx
@@ -1,11 +1,19 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './GoBackButton.css';
 
 const GoBackButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleGoBack = () => {
-    navigate('/file-upload'); // This takes you back to the previous page in the browser history
+    // List of routes that should navigate to home
+    const routesToHome = ['/file-upload', '/dashboard'];
+    
+    if (routesToHome.includes(location.pathname)) {
+      navigate('/home');
+    } else {
+      navigate(-1); // Go back one step in history
+    }
   };
 
   return (
